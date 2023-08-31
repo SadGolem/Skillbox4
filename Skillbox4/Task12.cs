4a74e03a0a20fd752055760a6309f1f1b348b715
Марина Зубцова <87470059+SadGolem@users.noreply.github.com>
2023-08-31 20:26:13 +0700

    Добавьте файлы проекта.

---
+++ Skillbox4/Task12.cs
@@ -0,0 +1,55 @@
+﻿//[--------------------Task1------------------------]
+
+
+Console.WriteLine("Введите количество строк");
+string rowsString = Console.ReadLine();
+Console.WriteLine("Введите количество столбцов");
+string columnsString = Console.ReadLine();
+
+int countRows = int.Parse(rowsString);
+int countColumns = int.Parse(columnsString);
+int sum = 0;
+
+int[,] matrx = new int[countRows, countColumns];
+
+Random rand = new Random();
+
+for (int i = 0; i < countRows; i++)
+{
+    for (int j = 0; j < countColumns; j++)
+    {
+        matrx[i, j] = rand.Next(20);
+        sum += matrx[i, j];
+        Console.Write(" " + matrx[i, j]);
+    }
+    Console.Write("\n");
+}
+Console.Write($"\nСумма: {sum} \n");
+
+//[--------------------Task2------------------------]
+
+int[,] matrx2 = new int[countRows, countColumns];
+int[,] matrx3 = new int[countRows, countColumns];
+
+Console.Write("\n");
+for (int i = 0; i < countRows; i++)
+{
+    for (int j = 0; j < countColumns; j++)
+    {
+        matrx2[i, j] = rand.Next(20);
+        sum += matrx2[i, j];
+        Console.Write(" " + matrx2[i, j]);
+    }
+    Console.Write("\n");
+}
+
+Console.Write("\n");
+for (int i = 0; i < countRows; i++)
+{
+    for (int j = 0; j < countColumns; j++)
+    {
+        matrx3[i, j] = matrx[i, j] + matrx2[i, j];
+        Console.Write(" " + matrx3[i, j]);
+    }
+    Console.Write("\n");
+}
---
+++ Skillbox4/Task3.cs
@@ -0,0 +1,53 @@
+﻿int n = 10;
+bool[] petry = new bool[n];
+
+
+Random rand = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    petry[i] = rand.Next(2) == 0 ? true : false;
+
+    if (petry[i])
+        Console.Write(" |" + "B" + "| ");
+    else
+        Console.Write(" |" + " " + "| ");
+}
+while (petry.Contains(true))
+{
+    Console.WriteLine("\nНажмите, чтобы продолжить...");
+    Console.ReadLine();
+
+    for (int i = 1; i < n-1; i++)
+    {
+        if (petry[i + 1] && petry[i - 1])
+        {
+            petry[i] = false;
+            Console.WriteLine($"\nБактерия {i} умерла от недостатка еды.\n");
+        }
+        else
+        {
+            petry[i + 1] = rand.Next(2) == 0;
+            petry[i - 1] = rand.Next(2) == 0;
+        }
+
+        if (!petry[i + 1] && !petry[i - 1])
+        {
+            petry[i] = false;
+            Console.WriteLine($"\nБактерия {i} умерла от недостатка внимания.\n");
+        }
+        else
+        {
+            petry[i + 1] = rand.Next(2) == 0 ;
+            petry[i - 1] = rand.Next(2) == 0;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (petry[i])
+            Console.Write(" |" + "B" + "| ");
+        else
+            Console.Write(" |" + " " + "| ");
+    }
+}
+
